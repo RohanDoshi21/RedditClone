3b7e9090a1f70573d57a6b45fc0c0fd49f9a3208
Rohan Doshi <rohan.doshi02@gmail.com>
2022-09-12 22:17:03 +0530

    Deleting Post

--- redditclone/urls.py
+++ redditclone/urls.py
@@ -21,6 +21,7 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts', views.PostList.as_view(), name='posts'),
+    path('api/posts/<int:pk>', views.PostRetrieveDestroy.as_view(), name='posts_destroy'),
     path('api/posts/<int:pk>/vote', views.VoteCreate.as_view(), name='vote'),
     path('api-auth/', include('rest_framework.urls')),
 ]
